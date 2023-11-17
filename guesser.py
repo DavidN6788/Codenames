9bc4934db7d325887f35dbc14f963f698f7ca54a
David Nguyen <nguyendavid332@gmail.com>
2023-11-17 22:43:13 +0000

    logic for guesser

---
+++ guesser.py
@@ -0,0 +1,14 @@
+class Gueser:
+    def __init__(self, team, board):
+        self.team = team
+        # Board object
+        self.board = board
+        self.curr_board = board.board
+
+    def make_guess(self, word):
+        try:
+            # find index of word and replace it
+            replace_index = self.curr_board[self.team].index(word)
+            self.curr_board[self.team][replace_index] = "------"
+        except:
+            print("Word not in board")
